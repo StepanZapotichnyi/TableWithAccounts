5e5ec3191208578b3f2782fd8d6ea67c70f94b54
StepanZapotichnyi <125075465+StepanZapotichnyi@users.noreply.github.com>
2024-03-12 10:54:24 +0100

    upadate account and add paginator

--- force-app/main/default/lwc/tableWithAccounts/tableWithAccounts.js
+++ force-app/main/default/lwc/tableWithAccounts/tableWithAccounts.js
@@ -1,26 +1,31 @@
 import { LightningElement, track } from 'lwc';
 import getDetails from '@salesforce/apex/AccountIntegrationController.getDetails';
+import patchDetails from '@salesforce/apex/AccountIntegrationController.patchDetails';
 import returnAccounts from '@salesforce/apex/AccountIntegrationController.returnAccounts';
+import updateReturnAccounts from '@salesforce/apex/AccountIntegrationController.updateReturnAccounts';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
 
 const pageSize = 7;   
 const columns = [
+    //add colum org
     {label: 'Name', fieldName : 'name'},
     {label: 'Account Number', fieldName : 'accountNumber', editable : true},
     {label: 'Ticker Symbol', fieldName : 'tickerSymbol', editable : true},
     {label: 'Rating', fieldName : 'rating', editable : true},
-    {label: 'Type', fieldName : 'type', editable : true}
+    {label: 'Type', fieldName : 'type', editable : true},
+    {label: 'Org', fieldName : 'org'}
 ];
-
 export default class TableWithAccounts extends LightningElement {
 
     @track listAcc = [];
     @track isLoading = false;
     @track page = 1;
     @track totalPage;
+    @track draftValues = [];
 
-    detailsPromise = [];
-    accountsPromise = [];
+    returnAccIds = [];
+    detailsIds = [];
 
     combinedAccounts = [];
     startingRecord = 1;
@@ -28,30 +33,26 @@ export default class TableWithAccounts extends LightningElement {
     totalRecordCount;
     columns = columns;
 
-    fildsItemValues = [];
 
-
-
-
-    connectedCallback() {
+    connectedCallback() { 
         this.loadingAccounts();
     }
 
     loadingAccounts() {
         this.isLoading = true;
+        
 
-        this.accountsPromise = returnAccounts();
-        this.detailsPromise = getDetails();
-        console.log('result 1+++' + this.detailsPromise);
-        console.log('result 1+++' + this.accountsPromise);
+        this.returnAccIds = returnAccounts();
+        this.detailsIds = getDetails();
+        console.log('result 1+++' + this.detailsIds);
+        console.log('result 1+++' + this.returnAccIds);
 
         Promise.all([getDetails(), returnAccounts()])
-            // async await
             .then(responses => {
-                this.detailsPromise = responses[0];
-                this.accountsPromise = responses[1];
-                console.log('result' + this.detailsPromise);
-                console.log('result' + this.accountsPromise);
+                this.detailsIds = responses[0].map(detail => detail.Id);
+                this.returnAccIds = responses[1].map(account => account.Id);
+                console.log('result' + this.detailsIds);
+                console.log('result' + this.returnAccIds);
 
                 responses.forEach(response => {
                     this.combinedAccounts = this.combinedAccounts.concat(response.map(acc => ({
@@ -60,7 +61,8 @@ export default class TableWithAccounts extends LightningElement {
                         accountNumber: acc.AccountNumber,
                         rating: acc.Rating,
                         tickerSymbol: acc.TickerSymbol,
-                        type: acc.Type
+                        type: acc.Type,
+                        org : this.checkingOrg(acc.Id)
                     })));
                 });
 
@@ -71,17 +73,25 @@ export default class TableWithAccounts extends LightningElement {
 
             })
             .catch(error => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error',
-                        message: 'Failed to retrieve accounts',
-                        variant: 'error'
-                    })
-                );
+                this.showToast('Success', 'Failed to retrieve accounts', 'error');
             })
             .finally(() => {
                 this.isLoading = false;
             });
+    }
+
+    checkingOrg(idAccount) {
+        
+        if (this.detailsIds.includes(idAccount)) {
+            let detailsOrg = 'Org First';
+            return detailsOrg;
+
+        }else if (this.returnAccIds.includes(idAccount)) {
+            let returnAccOrg = 'Org Second';
+            return returnAccOrg;
+        }
+        return 'unknown';
+    
     }
 
     prevHandel(event) {
@@ -104,9 +114,60 @@ export default class TableWithAccounts extends LightningElement {
         this.listAcc = this.combinedAccounts.slice(this.startingRecord, this.endingRecord);
     }
 
-    handleUpdate(event) {
-        this.fildsItemValues = event.detail.draftValues;
-        console.log(this.fildsItemValues);
+
+    handelSave(event) {
+        this.isLoading = true;
+        let draftValues = event.detail.draftValues;
+        let promisesAll = [];
+
+        // const accountIds = updatedFields.map(obj => obj.Id);            
+
+        draftValues.forEach(field => {
+            if (this.returnAccIds.includes(field.Id)) {
+                let returnUpdatedParam = [];
+                returnUpdatedParam.push(field);
+
+                if(returnUpdatedParam.length > 0) {
+                    promisesAll.push(updateReturnAccounts({accountData : returnUpdatedParam}));
+                }
+            }
+            else if (this.detailsIds.includes(field.Id)) {
+                let patchUpdatedParam = [];
+                patchUpdatedParam.push(field);
+                
+                if(patchUpdatedParam.length > 0) {
+                    let patchUpdatedParamString = JSON.stringify(patchUpdatedParam);
+                    promisesAll.push(patchDetails({accData : patchUpdatedParamString}));
+                }
+            }
+        });
+        
+        Promise.all(promisesAll)
+            .then (result =>  {
+                console.log('apex res ' + JSON.stringify(result));
+                this.combinedAccounts = [];
+                refreshApex(this.loadingAccounts());
+                this.showToast('Success', 'Records Updated Successfully!', 'success');
+            })
+            .catch (err => {
+                console.log('apex err ' + JSON.stringify(err));
+                this.showToast('Error', 'Changes not saved!', 'error');
+            }) 
+            .finally(() => {
+                this.isLoading = false;
+            });
     }
-    
+    handelCancel(event){
+        this.draftValues = []; 
+    }
+
+    showToast(title, message, variant) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(evt);
+    }     
+
 }
